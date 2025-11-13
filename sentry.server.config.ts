1554b3796cc4073bf210470c06844fc67d7d0653
Aryan <aryanshandilya10@gmail.com>
2025-11-13 19:46:28 +0530

    feat: added sentry

---
+++ sentry.server.config.ts
@@ -0,0 +1,27 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://130476b831fcdb84123f4ff73ae45a3e@o4510357910716416.ingest.us.sentry.io/4510357928476672",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+  integrations: [
+    // Add the Vercel AI SDK integration to sentry.server.config.ts
+    Sentry.vercelAIIntegration({
+      recordInputs: true,
+      recordOutputs: true,
+    }),
+    Sentry.consoleLoggingIntegration({ levels: ["log", "warn", "error"] }),
+  ],
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
--- src/inngest/function.ts
+++ src/inngest/function.ts
@@ -1,7 +1,8 @@
 import { inngest } from "./client";
 import { openai } from "@ai-sdk/openai";
-import { anthropic } from '@ai-sdk/anthropic';
-import { google } from '@ai-sdk/google';
+import * as Sentry from "@sentry/nextjs";
+import { anthropic } from "@ai-sdk/anthropic";
+import { google } from "@ai-sdk/google";
 
 import { generateText } from "ai";
 
@@ -9,27 +10,57 @@ export const execute = inngest.createFunction(
   { id: "execute-ai" },
   { event: "execute/ai" },
   async ({ step }) => {
-    const { steps: openaisteps } = await step.ai.wrap("openai-generate-text", generateText, {
-      model: openai("gpt-4o-mini"),
-      system: "You are a very helpful assisstant",
-      prompt: "Hey how are you doing my man? A QQ what is 2+2?",
-    });
+    console.warn("Hey this is a warning! Careful!");
+    console.error("This is error is meant for testing the Sentry logs");
+    Sentry.logger.info('User triggered test log', { log_source: 'sentry_test' })
+    const { steps: openaisteps } = await step.ai.wrap(
+      "openai-generate-text",
+      generateText,
+      {
+        model: openai("gpt-4o-mini"),
+        system: "You are a very helpful assisstant",
+        prompt: "Hey how are you doing my man? A QQ what is 2+2?",
+        experimental_telemetry: {
+          isEnabled: true,
+          recordInputs: true,
+          recordOutputs: true,
+        },
+      }
+    );
 
-    const { steps: geministeps } = await step.ai.wrap("gemini-generate-text", generateText, {
-      model: google("gemini-2.5-flash"),
-      system: "You are a very helpful assisstant",
-      prompt: "Hey how are you doing my man? A QQ what is 2+2?",
-    });
+    const { steps: geministeps } = await step.ai.wrap(
+      "gemini-generate-text",
+      generateText,
+      {
+        model: google("gemini-2.5-flash"),
+        system: "You are a very helpful assisstant",
+        prompt: "Hey how are you doing my man? A QQ what is 2+2?",
+        experimental_telemetry: {
+          isEnabled: true,
+          recordInputs: true,
+          recordOutputs: true,
+        },
+      }
+    );
 
-    const { steps: anthropicsteps } = await step.ai.wrap("anthropic-generate-text", generateText, {
-      model: anthropic("claude-sonnet-4-0"),
-      system: "You are a very helpful assisstant",
-      prompt: "Hey how are you doing my man? A QQ what is 2+2?",
-    });
+    const { steps: anthropicsteps } = await step.ai.wrap(
+      "anthropic-generate-text",
+      generateText,
+      {
+        model: anthropic("claude-sonnet-4-0"),
+        system: "You are a very helpful assisstant",
+        prompt: "Hey how are you doing my man? A QQ what is 2+2?",
+        experimental_telemetry: {
+          isEnabled: true,
+          recordInputs: true,
+          recordOutputs: true,
+        },
+      }
+    );
     return {
       openaisteps,
       geministeps,
-      anthropicsteps
+      anthropicsteps,
     };
   }
 );
