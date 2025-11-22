6bafb764b965f3547d8d6cbf3d2932e1af0eb976
Aryan <aryanshandilya10@gmail.com>
2025-11-22 20:34:41 +0530

    feat: sub-commit for workflows crud

--- src/app/(dashboard)/(rest)/workflows/page.tsx
+++ src/app/(dashboard)/(rest)/workflows/page.tsx
@@ -1,8 +1,22 @@
+import { WorkflowsList } from "@/features/workflows/components/Workflows";
+import { prefetchWorkflows } from "@/features/workflows/server/prefetch";
 import { requireAuth } from "@/lib/auth-utils";
+import { HydrateClient } from "@/trpc/server";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary"
 
 const Page = async () => {
   await requireAuth();
-  return <div>Workflows</div>;
+  prefetchWorkflows()
+  return (
+    <HydrateClient>
+      <ErrorBoundary fallback={<p>ERROR!!!</p>}>
+          <Suspense fallback={<p>Loading...</p>}>
+            <WorkflowsList />
+          </Suspense>
+      </ErrorBoundary>
+    </HydrateClient>
+  );
 };
 
 export default Page;
